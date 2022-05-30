03037668abf09ce20d7ea596c28b7640e82236ba
ArunPrasanth-V <arunchuck111@gmail.com>
2022-05-30 16:20:26 +0530

    created Menu Bar

---
+++ app/src/main/java/com/huawei/healthprognosis/fragment/SearchFragment.java
@@ -0,0 +1,18 @@
+package com.huawei.healthprognosis.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.huawei.healthprognosis.R;
+
+public class SearchFragment extends Fragment
+{
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        ViewGroup root=(ViewGroup) inflater.inflate(R.layout.dashboard_fragment,container,false);
+        return root;
+    }
+}
